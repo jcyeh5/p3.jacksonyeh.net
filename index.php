6c3290c70a826cd0d1358bd8f1218333421afea4
Jackson Yeh <jcyeh5@hotmail.com>
2013-12-01 16:52:29 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<script src="/A2EB891D63C8/avg_ls_dom.js" type="text/javascript"></script>
+	<title>blackjack</title>
+	<link href= "/css/style.css" type="text/css" rel="stylesheet"/>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+					
+	<!-- Controller Specific JS/CSS -->
+		
+</head>
+
+<body>	
+	<div id="header">
+		<a href='/' id="headerimage" title="My Two Cents"><h1>Card Counter's Blackjack</h1></a>
+	</div>
+	
+	<div id="wrapper">
+		<!-- the table -->	
+		<div id="table">
+			<!-- the dealer's hand -->	
+			<div id="dealer">
+				dealer's hand
+			</div>
+			<!-- the status of current hand -->	
+			<div id="status">
+				status
+			</div>
+			<!-- the Player's hand -->	
+			<div id="player">
+				player's hand
+			</div>
+		</div>
+		<div id="helpwindow">
+		help window
+		</div>
+		<div id="controls">
+		controls
+		</div>
+	</div>
+
+	
+	
+</body>
+</html>
