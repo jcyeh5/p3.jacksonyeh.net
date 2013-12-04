bebf133690770cceb283456ac5aa949edd2997a0
Jackson Yeh <jcyeh5@hotmail.com>
2013-12-03 20:27:46 -0500

    hit and deal cards to table

--- index.php
+++ index.php
@@ -19,62 +19,16 @@
 		<!-- the table -->	
 		<div id="table">
 			<!-- the dealer's hand -->	
-			<div id="dealer">
-				<div id="dealercard1" class="cardslot">
-					<img class="card" src="images/SK.png">
-				</div>
-				<div id="dealercard2" class="cardslot">
-					<img class="card" src="images/SA.png">				
-				</div>
-				<div id="dealercard3" class="cardslot">
-					<img class="card" src="images/CK.png">	
-				</div>
-				<div id="dealercard4" class="cardslot">
-					<img class="card" src="images/CA.png">	
-				</div>
-				<div id="dealercard5" class="cardslot">
-					<img class="card" src="images/HK.png">	
-				</div>
-				<div id="dealercard6" class="cardslot">
-					<img class="card" src="images/HA.png">	
-				</div>	
-				<div id="dealercard7" class="cardslot">
-					<img class="card" src="images/DA.png">	
-				</div>	
-				<div id="dealercard8" class="cardslot">
-					<img class="card" src="images/CARDBACK.png">	
-				</div>					
+			<div id="dealerhand">
+
 			</div>
 			<!-- the status of current hand -->	
 			<div id="status">
 				status
 			</div>
 			<!-- the Player's hand -->	
-			<div id="player">
-				<div id="playercard1" class="cardslot">
-					<img class="card" src="images/SQ.png">	
-				</div>
-				<div id="playercard2" class="cardslot">
-					<img class="card" src="images/SJ.png">	
-				</div>
-				<div id="playercard3" class="cardslot">
-					<img class="card" src="images/CQ.png">	
-				</div>
-				<div id="playercard4" class="cardslot">
-					<img class="card" src="images/CJ.png">	
-				</div>
-				<div id="playercard5" class="cardslot">
-					<img class="card" src="images/HQ.png">	
-				</div>
-				<div id="playercard6" class="cardslot">
-					<img class="card" src="images/HJ.png">	
-				</div>
-				<div id="playercard7" class="cardslot">
-					<img class="card" src="images/DQ.png">	
-				</div>
-				<div id="playercard8" class="cardslot">
-					<img class="card" src="images/CARDBACK.png">	
-				</div>					
+			<div id="playerhand">
+				
 			</div>
 		</div>
 		<div id="helpwindow">
@@ -85,7 +39,6 @@
 			<img id="hit_button" class="controlbuttons" class="rollover" src="images/hit.png" rel="images/hit_hover.png" alt="hit button">
 			<img id="stand_button" class="controlbuttons" src="images/stand.png" rel="images/stand_hover.png" alt="stand button" >
 			<img id="double_button" class="controlbuttons" src="images/double.png" rel="images/double_hover.png" alt="double button" >
-			<img id="split_button" class="controlbuttons" src="images/split.png" rel="images/split_hover.png" alt="split button"  >
 			<img id="deal_button" class="controlbuttons" src="images/deal.png" rel="images/deal_hover.png" alt="deal button"  >			
 		</div>
 	</div>
--- js/blackjack.js
+++ js/blackjack.js
@@ -1,7 +1,61 @@
 /*-------------------------------------------------------------------------------------------------
 Global Variables
 -------------------------------------------------------------------------------------------------*/
-var cards = 52;
+
+// deck of cards as an array
+var cards = [	"SA","S2","S3","S4","S5","S6","S7","S8","S9","S10","SJ","SQ","SK",
+				"CA","C2","C3","C4","C5","C6","C7","C8","C9","C10","CJ","CQ","CK",
+				"DA","D2","D3","D4","D5","D6","D7","D8","D9","D10","DJ","DQ","DK",
+				"HA","H2","H3","H4","H5","H6","H7","H8","H9","H10","HJ","HQ","HK",];
+
+var top = 0;			// index of the top card in deck
+var dealercards = []; 	// cards in dealer's hand
+var playercards = [];	// cards in player's hand
+
+
+function isEmpty(){
+	if (top == 52) {
+		return true;
+	}
+	else return false;
+};
+
+function draw(){
+	var card = cards[top];
+	top++;
+	return card;
+};
+
+function shuffle(){
+	for (var i=0; i<52; i++) {
+		var temp = cards[i];
+		var random = Math.floor(Math.random()*52);
+		cards[i] = cards[random];
+		cards[random] = temp;
+	}
+};
+	
+function handValue(array){
+	if (array.length == 0) return 0;
+	var value = 0;
+	for (var i=0; i<array.length; i++) {
+		var cardValue = getCardValue(array[i]);
+		value = value + cardValue;
+	}
+	return value;
+};
+
+function getCardValue(card) {
+	var rank = card.charAt(1);
+	if (rank == "A") return 11;
+	else if (rank == "K") return 10;
+	else if (rank == "Q") return 10;
+	else if (rank == "J") return 10;	
+	else if (rank == "1") return 10;
+	else return parseInt(rank);
+}
+
+
 
 /*-------------------------------------------------------------------------------------------------
 Buttons
@@ -11,11 +65,53 @@ $('.controlbuttons').click(function() {
 	 // Which control button was clicked?
 	 
 	 if (this.id == "hit_button") {
-	 cards--;
-	 console.log("cards left" + cards );
+		var handvalue = handValue(playercards);
+	 
+		if (handvalue == 0) {
+			$('#status').html("wait for the dealer to deal cards first!");
+		}
+		else if (handvalue > 0 && handvalue <= 21 ) {
+		
+			var newcard = draw();
+			var newcard_image = '<img class="card" src="images/' + newcard + '.png">';
+			$('#playerhand').append(newcard_image);
+			playercards.push(newcard);
+			var x = handValue(playercards);
+		}
+		else if (handvalue > 21) {
+			$('#status').html("you busted!!!");
+		}		
+
 	 }
-	 	 
+	 
+	 if (this.id == "deal_button") {	 
+
+		dealercards = [];
+		playercards = [];
+		$('#dealerhand').html("");
+		$('#playerhand').html("");		
+
+	 	for (var i=0; i<2; i++) {
+			var newcard = draw();
+			var newcard_image = '<img class="card" src="images/' + newcard + '.png">';
+			$('#playerhand').append(newcard_image);
+			playercards.push(newcard);
+		}
+	 
+		var newcard = draw();
+		var newcard_image = '<img class="card" src="images/' + newcard + '.png">';
+		$('#dealerhand').append(newcard_image);
+		dealercards.push(newcard);
+		
+		var newcard = draw();
+		var newcard_image = '<img class="card" src="images/CARDBACK.png">';
+		$('#dealerhand').append(newcard_image);
+		dealercards.push(newcard);	
+	}
+	 
 });	
+
+
 
 $('.controlbuttons').on( "mouseenter", function() {
 
@@ -43,14 +139,17 @@ $('.controlbuttons').on( "mouseleave", function() {
 
 
 $(document).ready(function($) {  
- console.log("roll"); 
+	console.log("document ready"); 
 
-//preload images 
-var cache = new Array(); //cycle through all rollover elements and add rollover img src to cache array 
-$(".rollover").each(function(){ 
-var cacheImage = document.createElement('img'); 
-cacheImage.src = $(this).attr('rel'); 
-cache.push(cacheImage); 
-}); 
-} );
+		
+	
+ 
+	//preload images 
+	var cache = new Array(); //cycle through all rollover elements and add rollover img src to cache array 
+	$(".rollover").each(function(){ 
+	var cacheImage = document.createElement('img'); 
+	cacheImage.src = $(this).attr('rel'); 
+	cache.push(cacheImage); 
+	}); 
+});
 //- See more at: http://www.grasmash.com/article/simple-jquery-script-swapping-images-hoverrollover#sthash.8op7JXg9.dpuf
